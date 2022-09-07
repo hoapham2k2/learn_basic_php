48e391ef9451201ee210c906ff96f01c4c7616ec
Hoapham <20520995@gm.uit.edu.vn>
2022-09-07 12:23:16 +0700

    first init

---
+++ .history/index_20220907115833.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Index</h1>
+    <?php
+    $so_nguyen = 10;
+    $so_thuc = 10.5;
+    $chuoi = "Hello World";
+    $mang = array(1, 2, 3, 4, 5);
+    $mang2 = array("a" => $mang, "b" => 2, "c" => 3);
+    $boolean_true = true;
+    $boolean_false = false;
+    $varible_null = null;
+    echo $so_nguyen;
+    echo "<br>";
+    echo $so_thuc;
+    echo "<br>";
+    echo $chuoi;
+    echo "<br>";
+    echo $mang[0];
+    echo "<br>";
+    echo $mang2["a"];
+    echo "<br>";
+    echo $boolean_true;
+    echo "<br>";
+    echo $boolean_false;
+    echo "<br>";
+    echo $varible_null;
+    echo "<br>";
+    ?>
+</body>
+
+</html>
---
+++ .history/index_20220907120828.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Index</h1>
+    <?php
+    $so_nguyen = 10;
+    $so_thuc = 10.5;
+    $chuoi = "Hello World";
+    $variable_object = new stdClass();
+    $mang = array(3 => 1, 1 => 2, 3, 4, 5);
+    $mang2 = array("a" => $mang, "b" => 2, "c" => 3);
+    $boolean_true = true;
+    $boolean_false = false;
+    $varible_null = null;
+    echo "so nguyen: ", $so_nguyen;
+    echo "<br>";
+    echo "so thuc: ", $so_thuc;
+    echo "<br>";
+    echo "chuoi: ", $chuoi;
+    echo "<br>";
+    echo "phan tu trong mang: ", $mang[0];
+    echo "<br>";
+    echo "mang 1 la phan tu cua mang 2: ";
+    print_r($mang2["a"]);
+    echo "<br>";
+    echo "gia tri bool true: ", $boolean_true;
+    echo "<br>";
+    echo "gia tri bool false: ", $boolean_false;
+    echo "<br>";
+    echo "gia tri null: ", $varible_null;
+    echo "<br>";
+    ?>
+</body>
+
+</html>
